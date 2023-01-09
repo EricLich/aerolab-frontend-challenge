ff3c8ed1845178391b7fe821b386cc70b4407bd7
Eric Lich <eric.lichtensztein@aleia.io>
2023-01-08 23:56:15 -0300

    add mobile styles to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import HeroIllustration from "./HeroIllustration";
 
 const Hero = () => {
   return (
-    <section className="flex w-full justify-center lg:justify-between items-center md:mt-10 lg:mt-[112px] lg:gap-8 md:px-8">
+    <section className="flex w-full justify-center lg:justify-between items-center md:mt-10 lg:mt-[112px] lg:gap-8 px-4 md:px-8">
       <HeroInfo />
       <HeroIllustration />
     </section>
--- components/HeroInfo.tsx
+++ components/HeroInfo.tsx
@@ -5,8 +5,6 @@ import React, { ReactNode } from "react";
 
 import CommonButton from "./CommonButton";
 
-const lgHeroFontsize = "200px";
-
 const HeroInfo = () => {
   const arrowDownIcon: ReactNode = (
     <Image src="assets/icons/arrow-down.svg" width={24} height={24} alt="view all products" />
@@ -14,22 +12,22 @@ const HeroInfo = () => {
 
   return (
     <div className="flex flex-col items-center lg:items-start">
-      <p className="md:text-mdDefaultCaps lg:text-lgDefaultCaps uppercase md:font-weightDefaultCaps lg:font-weightDefault leading-defaultCaps text-neutral600 mb-2">
+      <p className="text-smDefaultCaps md:text-mdDefaultCaps lg:text-lgDefaultCaps uppercase font-weightDefaultCaps md:font-weightDefaultCaps lg:font-weightDefault leading-defaultCaps text-neutral600 mb-2">
         explore the
       </p>
       <h1
-        className={`flex flex-col md:items-center lg:items-start md:text-mdHero lg:text-[min(15vw,200px,200px)] uppercase font-weightHero leading-hero mb-6`}
+        className={`flex flex-col md:items-center lg:items-start text-smHero md:text-mdHero lg:text-[min(15vw,200px,200px)] uppercase font-weightHero leading-hero mb-6`}
       >
         <span className="gradientDefault bg-clip-text text-transparent">tech</span>
         <span className="text-neutral900">zone</span>
       </h1>
-      <h3 className="w-full md:max-w-[305px] lg:max-w-[523px] text-center lg:text-left md:text-mdDefault lg:text-lgDefault font-weightDefault leading-default text-neutral600 md:mb-10 lg:mb-14">
+      <h3 className="w-full max-w-[305px] lg:max-w-[523px] text-center lg:text-left md:text-mdDefault lg:text-lgDefault font-weightDefault leading-default text-neutral600 mb-10 lg:mb-14">
         Here you&apos;ll be able to exchange all of your hard-earned Aeropoints and exchange them for cool tech.
       </h3>
       <CommonButton
         firstText="VIEW ALL PRODUCTS"
         rightIcon={arrowDownIcon}
-        extraClasses="md:max-w-[303px] lg:max-w-[318px] !rounded-3xl"
+        extraClasses="max-w-[303px] lg:max-w-[318px] !rounded-3xl"
         extraHeight
       />
     </div>
