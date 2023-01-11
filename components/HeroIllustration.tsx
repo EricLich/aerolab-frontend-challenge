e101834b689cd81974ceefbc359ffb2dc2fa9d6e
EricLich <erick_lich@hotmail.com>
2023-01-11 11:49:18 -0300

    improve hero image desktop

--- components/HeroIllustration.tsx
+++ components/HeroIllustration.tsx
@@ -6,15 +6,14 @@ import React from "react";
 const HeroIllustration = () => {
   return (
     <div className="relative hidden lg:flex lg:w-full lg:h-full lg:max-w-[722px] h-[600px] aspect-square justify-center items-center">
-      <div className="h-full w-full gradientHeroIllustration50Opacity rounded-[104px] shadow-md overflow-visible">
-        <Image
-          src="/assets/illustrations/hero-desktop.png"
-          width={3000}
-          height={3000}
-          alt="hero illustration"
-          className="absolute bottom-0 h-[135%] lg:max-w-[722px] w-full left-0 right-0 ml-auto mr-auto"
-        />
-      </div>
+      <div className="absolute h-full w-full gradientHeroIllustration50Opacity rounded-[104px] shadow-md overflow-visible"></div>
+      <Image
+        src="/assets/illustrations/hero-desktop.png"
+        width={3000}
+        height={3000}
+        alt="hero illustration"
+        className="absolute bottom-0 object-cover h-[125%]"
+      />
     </div>
   );
 };
