2784be70d301ce46a1314c31cc902d74f0180166
EricLich <erick_lich@hotmail.com>
2023-01-11 23:21:23 -0300

    add sort by btns

--- components/SortBy.tsx
+++ components/SortBy.tsx
@@ -2,8 +2,20 @@
 
 import React from "react";
 
+import { possibleSortingOptions } from "../utils/types";
+import SortByOptionButton from "./SortByOptionButton";
+
 const SortBy = () => {
-  return <div>SortBy</div>;
+  return (
+    <div className="flex items-center">
+      <p className="hidden lg:block lg:text-lgDefault lg:font-weightDefault text-neutral600 mr-4">Sort by:</p>
+      <div className="flex items-center lg:gap-3">
+        {possibleSortingOptions.map((option) => (
+          <SortByOptionButton key={option} option={option} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SortBy;
---
+++ components/SortByOptionButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React, { useMemo } from "react";
+import { useProductsContext } from "../contexts/productsContext";
+import { PossibleSortingOptions } from "../utils/types";
+
+type SortByOptionButtonProps = {
+  option: PossibleSortingOptions;
+};
+
+const SortByOptionButton: React.FC<SortByOptionButtonProps> = ({ option }) => {
+  const { sortingOption, setSortingOption } = useProductsContext();
+
+  const dynamicStyling = useMemo((): [string, string] => {
+    if (sortingOption === option) {
+      return ["gradientDefault", "text-neutral100"];
+    } else {
+      return ["bg-brandLight hover:bg-brandLight2 duration-100", "bg-clip-text text-transparent gradientDefault"];
+    }
+  }, [sortingOption]);
+
+  return (
+    <button
+      onClick={() => setSortingOption(option)}
+      className={`text-center ${dynamicStyling[0]} rounded-xl md:px-4 lg:h-[43px] py-1 text-smDefaultSmall md:text-mdDefault lg:text-lgDefault font-weightDefault`}
+    >
+      <span className={dynamicStyling[1]}>{option}</span>
+    </button>
+  );
+};
+
+export default SortByOptionButton;
