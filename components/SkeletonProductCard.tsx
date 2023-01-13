f54471e4712ef61beb23640d0788fa9ff30bb529
EricLich <erick_lich@hotmail.com>
2023-01-13 17:59:24 -0300

    add kite image on card skeleton

--- components/SkeletonProductCard.tsx
+++ components/SkeletonProductCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -8,8 +9,15 @@ const SkeletonProductCard = () => {
   return (
     <article className="w-full flex flex-col h-[506px] gap-4">
       <section className="flex flex-col border border-neutral300 rounded-lg h-[431px] overflow-hidden">
-        <Skeleton className="w-full h-[345px]" baseColor="#E6EDF7" highlightColor="#DAE4F2" />
-
+        <div className="w-full h-[345px] flex items-center justify-center border-b border-neutral300">
+          <Image
+            src="/assets/illustrations/aerolab-logo-loading.png"
+            alt={`kite image`}
+            width={95}
+            height={95}
+            className="w-[95px] "
+          />
+        </div>
         <div className="h-auto py-3 px-6 flex flex-col items-start justify-center">
           <Skeleton className="h-[20px]  !w-[180px] mb-1" baseColor="#E6EDF7" highlightColor="#DAE4F2" />
 
