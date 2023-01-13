f6e082127c9d2085b0e84a086fa0b0ce8f44e225
EricLich <erick_lich@hotmail.com>
2023-01-13 17:38:40 -0300

    finetune skeleton

--- components/SkeletonProductCard.tsx
+++ components/SkeletonProductCard.tsx
@@ -10,15 +10,10 @@ const SkeletonProductCard = () => {
       <section className="flex flex-col border border-neutral300 rounded-lg h-[431px] overflow-hidden">
         <Skeleton className="w-full h-[345px]" baseColor="#E6EDF7" highlightColor="#DAE4F2" />
 
-        <div className="h-[160px] lg:py-3 px-6 flex flex-col items-start justify-center">
-          <Skeleton
-            count={1}
-            className="h-[20px] !w-[180px] bigScreen:w-[228px] mb-1"
-            baseColor="#E6EDF7"
-            highlightColor="#DAE4F2"
-          />
+        <div className="h-auto py-3 px-6 flex flex-col items-start justify-center">
+          <Skeleton className="h-[20px]  !w-[180px] mb-1" baseColor="#E6EDF7" highlightColor="#DAE4F2" />
 
-          <Skeleton count={1} className="h-[18px] !w-[150px]" baseColor="#E6EDF7" highlightColor="#DAE4F2" />
+          <Skeleton className="h-[18px]  !w-[120px] " baseColor="#E6EDF7" highlightColor="#DAE4F2" />
         </div>
       </section>
       <Skeleton className="h-[56px] md:h-[59px] !rounded-2xl" baseColor="#E6EDF7" highlightColor="#DAE4F2" />
